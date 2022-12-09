f63c9c7c73c6c3189ead8794be931de6d8f6ac8d
Huang Runhua <45262463+HuangRunHua@users.noreply.github.com>
2022-12-09 19:37:58 +0800

    Initial Commit

---
+++ HashTagText/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HashTagText
+//
+//  Created by Huang Runhua on 12/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HashTagText/HashTagTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  HashTagTextApp.swift
+//  HashTagText
+//
+//  Created by Huang Runhua on 12/9/22.
+//
+
+import SwiftUI
+
+@main
+struct HashTagTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
