880e34777e10697a5881b1b924bdbeba4d4dd748
Huang Runhua <45262463+HuangRunHua@users.noreply.github.com>
2022-12-13 21:06:56 +0800

    update view

--- HashTagText/ContentView.swift
+++ HashTagText/ContentView.swift
@@ -11,10 +11,23 @@ struct ContentView: View {
     
     private let text: String = "The largest #strikes in #Britain for a decade caused the loss of 417,000 working hours during October, according to the Office for National Statistics. A further 1m hours are expected to be lost in December as rail workers, postal workers, nurses and teachers demand better pay and conditions. Inflation and soaring energy costs have created a #cost-of-living crisis in the country."
     
+    @State private var showAlert: Bool = false
+    @State private var selectedKeyword: String = ""
+    
     var body: some View {
         HashtagText(text)
             .modifier(HashtagTextModifier())
             .padding()
+            .onOpenURL { url in
+                let string = url.absoluteString
+                if let keyword = string.split(separator: "//").last {
+                    self.selectedKeyword = String(keyword)
+                    self.showAlert.toggle()
+                }
+            }
+            .sheet(isPresented: $showAlert) {
+                HashtagDetailView(hashtag: $selectedKeyword)
+            }
     }
 }
 
--- HashTagText/HashTagTextApp.swift
+++ HashTagText/HashTagTextApp.swift
@@ -10,23 +10,9 @@ import SwiftUI
 @main
 struct HashTagTextApp: App {
     
-    @State private var showAlert: Bool = false
-    @State private var selectedKeyword: String = ""
-    
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .onOpenURL { url in
-                    let string = url.absoluteString
-                    if let keyword = string.split(separator: "//").last {
-                        self.selectedKeyword = String(keyword)
-                        print(self.selectedKeyword)
-                        self.showAlert.toggle()
-                    }
-                }
-                .sheet(isPresented: $showAlert) {
-                    HashtagDetailView(hashtag: $selectedKeyword)
-                }
         }
     }
 }
